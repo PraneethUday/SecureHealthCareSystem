ede409ad97372445251534d403c5d8dbf50beb83
Pradeep Sasidharan <pradeepsasidharank19592004@gmail.com>
2026-01-28 09:50:14 +0530

    Update : Updated Chatbot

---
+++ app/dashboard/patient/layout.tsx
@@ -0,0 +1,14 @@
+import ChatWidget from "../components/chatbot/ChatWidget";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <ChatWidget />
+    </>
+  );
+}
